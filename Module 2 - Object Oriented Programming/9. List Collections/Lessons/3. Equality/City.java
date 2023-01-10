c86f69ef3d7262b8529e8a2ec55149ca017b3bfc
Callie <c.p.vanhuyssteen@gmail.com>
2023-01-10 20:39:54 +0200

    Override equals

--- Module 2 - Object Oriented Programming/9. List Collections/Lessons/3. Equality/City.java
+++ Module 2 - Object Oriented Programming/9. List Collections/Lessons/3. Equality/City.java
@@ -30,5 +30,7 @@ public class City {
         this.population = population;
     }
 
+    
+
 
 }
--- Module 2 - Object Oriented Programming/9. List Collections/Lessons/3. Equality/Main.java
+++ Module 2 - Object Oriented Programming/9. List Collections/Lessons/3. Equality/Main.java
@@ -4,11 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         City paris = new City("Paris", 2161000);
-        City copy = paris;
 
-        System.out.println(paris.equals(copy));
-        copy.setPopulation(2059003);
-        paris.setPopulation(3129003);
 
         ArrayList<City> cities = new ArrayList<>();
         cities.add(new City("Paris", 2161000));
